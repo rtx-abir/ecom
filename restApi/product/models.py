1701a502cee9bfa0b1644a01747277248b386333
Abir Deb <Debabir91@gmail.com>
2020-12-19 17:38:10 -0500

    image field and serializer changes

--- restApi/product/models.py
+++ restApi/product/models.py
@@ -4,13 +4,14 @@ from restApi.category.models import Category
 
 # Create your models here.
 class product(models.Model):
-    categories = models.ManyToManyField(Category)
+    # category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank= True, null= True)
+    category = models.ManyToManyField(Category)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     price = models.DecimalField(decimal_places=2,max_digits=10)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, blank= True)
     stock = models.IntegerField()
-    image = models.CharField(max_length=400)
+    image = models.ImageField(upload_to = "images/", blank = True, null = True)   # creates new folder inside media in settings.py
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- restApi/product/serializers.py
+++ restApi/product/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 
 from .models import product
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length = None, allow_empty_file = False, allow_null= True, required = False)
     class Meta:
         model = product
         fields = '__all__'
+        depth = 1
