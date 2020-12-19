bf01cc72e1a80f9616351cf0eef94e9c69663885
Abir Deb <Debabir91@gmail.com>
2020-12-19 18:08:38 -0500

    product serializer change

--- restApi/product/serializers.py
+++ restApi/product/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import product
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     image = serializers.ImageField(max_length = None, allow_empty_file = False, allow_null= True, required = False)
     class Meta:
         model = product
